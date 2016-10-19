7263085f148ec52bbd99a46c99614db9303c6cf0
Raggaer <nakotoffana@gmail.com>
2016-10-19 17:36:47 +0200

    Better formatting

--- app/app.go
+++ app/app.go
@@ -4,17 +4,17 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/raggaer/castro/app/util"
 	"github.com/urfave/negroni"
 )
 
 // Start the main execution point for Castro
 func Start() {
-	setLogger()
 	mux := httprouter.New()
 	n := negroni.Classic()
 	n.UseHandler(mux)
-	l.Error("hola")
+	util.Logger.Infof("Starting Castro http server on port: %v", 8080)
 	if err := http.ListenAndServe(":8080", n); err != nil {
-
+		util.Logger.Errorf("Cannot start Castro http server: %v", err)
 	}
 }
---
+++ app/util/logger.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"os"
+
+	"github.com/op/go-logging"
+)
+
+var (
+	// Logger main logger instance of the app
+	Logger = logging.MustGetLogger("castro")
+
+	// Format for all util messages
+	utilFormat = logging.MustStringFormatter(
+		`%{color}%{time:15:04:05} - %{level:.5s}:%{color:reset} %{message}`,
+	)
+
+	// Format for all error messages
+	errFormat = logging.MustStringFormatter(
+		`%{color}%{time:15:04:05} %{shortfunc} - %{level:.5s}:%{color:reset} %{message}`,
+	)
+)
+
+func init() {
+	// Create backend for logging
+	backend := logging.NewLogBackend(os.Stderr, "", 0)
+
+	// For messages written to backend we want to add some additional
+	// information
+	backendFormatter := logging.NewBackendFormatter(backend, utilFormat)
+
+	// Set the backends to be used.
+	logging.SetBackend(backendFormatter)
+}
