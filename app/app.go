ec2f857e4ecd9fd77245fa8f3ef93353784901f1
Raggaer <nakotoffana@gmail.com>
2016-10-19 18:00:01 +0200

    toml config

--- app/app.go
+++ app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -8,8 +10,23 @@ import (
 	"github.com/urfave/negroni"
 )
 
+var (
+	// Config variable to hold the main
+	// configuration file
+	Config = &util.Config{}
+)
+
 // Start the main execution point for Castro
 func Start() {
+	// Load the configration file
+	file, err := ioutil.ReadFile("config.toml")
+	if err != nil {
+		util.Logger.Fatalf("Cannot read configuration file: %v", err)
+	}
+	if err = util.LoadConfig(string(file), Config); err != nil {
+		util.Logger.Fatalf("Cannot read configuration file: %v", err)
+	}
+
 	// Create the http router instance
 	mux := httprouter.New()
 
@@ -21,11 +38,11 @@ func Start() {
 	n.UseHandler(mux)
 
 	// Everything set lets start the http server
-	util.Logger.Infof("Starting Castro http server on port: %v", 8080)
+	util.Logger.Infof("Starting Castro http server on port :%v", Config.Port)
 
-	if err := http.ListenAndServe(":8080", n); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%v", Config.Port), n); err != nil {
 		// This should only happen when a port is
 		// already in use
-		util.Logger.Errorf("Cannot start Castro http server: %v", err)
+		util.Logger.Fatalf("Cannot start Castro http server: %v", err)
 	}
 }
--- app/util/config.go
+++ app/util/config.go
@@ -1,7 +1,18 @@
 package util
 
+import "github.com/BurntSushi/toml"
+
 // Config struct used for the main Castro
-// config file
+// config file TOML file
 type Config struct {
 	Port int
+}
+
+// LoadConfig loads the configuration file to
+// the given interface
+func LoadConfig(data string, dest interface{}) error {
+	if _, err := toml.Decode(data, dest); err != nil {
+		return err
+	}
+	return nil
 }
