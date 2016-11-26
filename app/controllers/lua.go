639d94eb44d14a54a119b6b62deef0d8e3dd623a
Raggaer <nakotoffana@gmail.com>
2016-11-26 22:25:38 +0100

    Handling lua page error

--- app/controllers/lua.go
+++ app/controllers/lua.go
@@ -13,6 +13,6 @@ func LuaPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	luaState := lua.Pool.Get()
 	defer lua.Pool.Put(luaState)
 	if err := luaState.DoFile("pages/" + ps.ByName("page") + ".lua"); err != nil {
-		util.Logger.Error(err.Error())
+		util.Logger.Errorf("Cannot execute %v: %v\n", ps.ByName("page"), err)
 	}
 }
