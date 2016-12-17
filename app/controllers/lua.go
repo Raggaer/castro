121cf02209616e7e351ce84d7b433a6c7131f14b
Raggaer <nakotoffana@gmail.com>
2016-12-18 00:46:54 +0100

    Subtopic error log

--- app/controllers/lua.go
+++ app/controllers/lua.go
@@ -13,6 +13,13 @@ func LuaPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	luaState := lua.Pool.Get()
 	defer lua.Pool.Put(luaState)
 	if err := luaState.DoFile("pages/" + ps.ByName("page") + ".lua"); err != nil {
-		util.Logger.Errorf("Cannot execute %v: %v\n", ps.ByName("page"), err)
+		if util.Config.IsDev() {
+			util.Logger.Errorf("Cannot execute %v: %v\n", ps.ByName("page"), err)
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		w.WriteHeader(400)
+		w.Write([]byte("Cannot execute the given subtopic"))
 	}
 }
