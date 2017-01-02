cfc1893b288f9d428abbfa5dfb64e97a78493ace
Raggaer <nakotoffana@gmail.com>
2017-01-02 14:50:28 +0100

    Async app init

--- app/app.go
+++ app/app.go
@@ -13,10 +13,35 @@ import (
 	"github.com/raggaer/castro/app/util"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // Start the main execution point for Castro
 func Start() {
+	// Wait for all goroutines to make their work
+	wait := &sync.WaitGroup{}
+
+	// Wait for 8 tasks
+	wait.Add(8)
+
+	// Execute our tasks
+	go func(wait *sync.WaitGroup) {
+		loadAppConfig(wait)
+		loadLUAConfig(wait)
+		connectDatabase(wait)
+		migrateDatabase(wait)
+	}(wait)
+	go createCache(wait)
+	go loadWidgetList(wait)
+	go appTemplates(wait)
+	go widgetTemplates(wait)
+
+
+	// Wait for the tasks
+	wait.Wait()
+}
+
+func loadAppConfig(wg *sync.WaitGroup) {
 	// Load the TOML configuration file
 	file, err := ioutil.ReadFile("config.toml")
 	if err != nil {
@@ -26,11 +51,31 @@ func Start() {
 		util.Logger.Fatalf("Cannot read configuration file: %v", err)
 	}
 
+	// Tell the wait group we are done
+	wg.Done()
+}
+
+func loadLUAConfig(wg *sync.WaitGroup) {
 	// Load the LUA configuration file
 	if err := lua.LoadConfig(util.Config.Datapack, lua.Config); err != nil {
 		util.Logger.Fatalf("Cannot read lua configuration file: %v", err)
 	}
 
+	// Tell the wait group we are done
+	wg.Done()
+}
+
+func createCache(wg *sync.WaitGroup) {
+	// Create a new cache instance with the given options
+	// first parameter is the default item duration on the cache
+	// second parameter is the tick time to purge all dead cache items
+	util.Cache = cache.New(time.Duration(util.Config.Cache.Default), time.Duration(util.Config.Cache.Purge))
+
+	// Tell the wait group we are done
+	wg.Done()
+}
+
+func loadWidgetList(wg *sync.WaitGroup) {
 	// Load widget list
 	wList, err := util.LoadWidgetList("widgets/")
 
@@ -41,11 +86,11 @@ func Start() {
 	// Assign widget list to global variable
 	util.WidgetList = wList
 
-	// Create a new cache instance with the given options
-	// first parameter is the default item duration on the cache
-	// second parameter is the tick time to purge all dead cache items
-	util.Cache = cache.New(time.Duration(util.Config.Cache.Default), time.Duration(util.Config.Cache.Purge))
+	// Tell the wait group we are done
+	wg.Done()
+}
 
+func appTemplates(wg *sync.WaitGroup) {
 	// Create application template
 	util.Template = util.NewTemplate("castro")
 
@@ -58,6 +103,11 @@ func Start() {
 		util.Logger.Fatalf("Cannot load templates: %v", err)
 	}
 
+	// Tell the wait group we are done
+	wg.Done()
+}
+
+func widgetTemplates(wg *sync.WaitGroup) {
 	// Create widget template
 	util.WidgetTemplate = util.NewTemplate("widget")
 
@@ -68,15 +118,30 @@ func Start() {
 		util.Logger.Fatalf("Cannot load widget templates: %v", err)
 	}
 
+	// Tell the wait group we are done
+	wg.Done()
+}
+
+func connectDatabase(wg *sync.WaitGroup) {
+	var err error
+
 	// Connect to the MySQL database
 	if database.DB, err = database.Open(lua.Config.MySQLUser, lua.Config.MySQLPass, lua.Config.MySQLDatabase); err != nil {
 		util.Logger.Fatalf("Cannot connect to MySQL database: %v", err)
 	}
 
+	// Tell the wait group we are done
+	wg.Done()
+}
+
+func migrateDatabase(wg *sync.WaitGroup) {
 	// Migrate database models
 	if err := database.DB.AutoMigrate(&models.Article{}).Error; err != nil {
 		util.Logger.Fatalf("Cannot migrate database models: %v", err)
 	}
+
+	// Tell the wait group we are done
+	wg.Done()
 }
 
 func templateFuncs() template.FuncMap {
