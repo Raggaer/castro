03545577116d9273d42d8a6ab0c219b113b761d8
Raggaer <nakotoffana@gmail.com>
2017-02-10 00:43:32 +0100

    Create guild

--- pages/community/guilds/list/get.lua
+++ pages/community/guilds/list/get.lua
@@ -8,8 +8,10 @@ end
 
 data.list = db:query("SELECT a.name, b.name as owner, a.creationdata FROM guilds a, players b WHERE a.ownerid = b.id ORDER BY a.creationdata DESC")
 
-for _, val in pairs(data.list) do
-    val.creation = time:parseUnix(tonumber(val.creationdata))
+if data.list ~= nil then
+    for _, val in pairs(data.list) do
+        val.creation = time:parseUnix(tonumber(val.creationdata))
+    end
 end
 
 data["validationError"] = session:getFlash("validationError")
--- pages/community/guilds/list/post.lua
+++ pages/community/guilds/list/post.lua
@@ -25,3 +25,6 @@ if db:singleQuery("SELECT 1 FROM guild_membership WHERE player_id = ?", characte
 end
 
 local guild_id = db:execute("INSERT INTO guilds (name, ownerid, creationdata, motd) VALUES (?, ?, ?, ?)", http.postValues["guild-name"], character.id, os.time(), "Guild leader must edit this text")
+local leader_id = db:singleQuery("SELECT id FROM guild_ranks WHERE guild_id = ? AND level = 3", guild_id)
+
+db:execute("INSERT INTO guild_membership (player_id, guild_id, rank_id) VALUES (?, ?, ?)", character.id, guild_id, leader_id.id)
