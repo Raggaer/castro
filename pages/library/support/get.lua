263829d98d54262147a30d41f2f05f621757d8ab
Raggaer <nakotoffana@gmail.com>
2017-02-26 21:07:36 +0100

    Support page

---
+++ pages/library/support/get.lua
@@ -0,0 +1,17 @@
+function get()
+    local data = {}
+    local groups = xml:unmarshalFile(serverPath .. "/data/XML/groups.xml")
+
+    data.list = {}
+
+    for _, group in pairs(groups.groups.group) do
+        if tonumber(group["-access"]) >= 1 then
+            data.list[group["-name"]] = db:query("SELECT name, lastlogin FROM players WHERE group_id = ?", group["-id"])
+            for _, player in pairs(data.list[group["-name"]]) do
+                player.lastlogin = time:parseUnix(player.lastlogin)
+            end
+        end
+    end
+
+    http:render("support.html", data)
+end
