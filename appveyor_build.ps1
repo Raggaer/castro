8032541d8a7d7a4c0c01baf910c006182a169296
Raggaer <nakotoffana@gmail.com>
2017-03-01 21:54:56 +0100

    appveyor

---
+++ appveyor_build.ps1
@@ -0,0 +1,13 @@
+echo "Building for Windows"
+$env:GOOS = "windows"
+
+$winCommand = 'go build -o buildOutput\castro.exe'
+
+iex $winCommand
+
+echo "Building for Linux"
+$env:GOOS = "linux"
+
+$linuxCommand = 'go build -o buildOutput\castro'
+
+iex $linuxCommand
