d52095f1397f05106c787f10dac374770c90f6f2
Raggaer <nakotoffana@gmail.com>
2017-03-02 00:08:40 +0100

    Set compilation variables

--- appveyor_build.ps1
+++ appveyor_build.ps1
@@ -1,3 +1,5 @@
+$date = (Get-Date).AddDays(-1).ToString('MM-dd-yyyy HH:mm:ss')
+
 $initCommand = 'go get'
 
 iex $initCommand
@@ -5,7 +7,7 @@ iex $initCommand
 echo "Building for Windows"
 $env:GOOS = "windows"
 
-$winCommand = 'go build -o buildOutput\castro.exe'
+$winCommand = 'go build -o buildOutput\castro.exe -ldflags "-X main.VERSION=1.0.0.{build} -X main.BUILD_DATE=$date"'
 
 iex $winCommand
 
