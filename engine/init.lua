342570b1f753106abfea6267e3b2900169ae7754
Raggaer <nakotoffana@gmail.com>
2017-03-18 01:15:49 +0100

    development mode message

--- engine/init.lua
+++ engine/init.lua
@@ -1,2 +1,5 @@
 -- This file will be executed at start-up
+if app.Mode == "dev" then
+    print("Running on development mode. Never have development mode open to the public")
+end
 print("Castro started at " .. time:parseUnix(os.time()).Result)
