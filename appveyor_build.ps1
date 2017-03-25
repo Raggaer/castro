fe2819dae5d44de8594458ef9b27f3294e3a64f7
Raggaer <nakotoffana@gmail.com>
2017-03-25 14:13:20 +0100

    Use commit hash as main version

--- appveyor_build.ps1
+++ appveyor_build.ps1
@@ -1,4 +1,5 @@
 $date = (Get-Date).AddDays(-1).ToString('MM-dd-yyyy_HH:mm:ss')
+$version = git rev-parse HEAD
 
 $initCommand = 'go get'
 
@@ -7,18 +8,18 @@ iex $initCommand
 echo "Building for Windows amd64"
 $env:GOOS = "windows"
 
-$winCommand = 'go build -v -o buildOutput\castro_win_amd64.exe -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
+$winCommand = 'go build -v -o buildOutput\castro_win_amd64.exe -ldflags "-X main.VERSION=$version -X main.BUILD_DATE=$date"'
 
 iex $winCommand
 
 echo "Building for Linux amd64"
 $env:GOOS = "linux"
 
-$linuxCommand = 'go build -v -o buildOutput\castro_linux_amd64 -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
+$linuxCommand = 'go build -v -o buildOutput\castro_linux_amd64 -ldflags "-X main.VERSION=$version -X main.BUILD_DATE=$date"'
 
 iex $linuxCommand
 
-$linuxCommand = 'go build -v -o buildOutput\castro_linux_arm64 -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
+$linuxCommand = 'go build -v -o buildOutput\castro_linux_arm64 -ldflags "-X main.VERSION=$version -X main.BUILD_DATE=$date"'
 
 echo "Building for Linux arm64"
 $env:GOOS = "linux"
