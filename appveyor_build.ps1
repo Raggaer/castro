2c3e38d01c218c26493e4bdeb8a8df6f057832a6
Raggaer <nakotoffana@gmail.com>
2017-03-25 13:41:25 +0100

    Build for linux arm64

--- appveyor_build.ps1
+++ appveyor_build.ps1
@@ -4,16 +4,26 @@ $initCommand = 'go get'
 
 iex $initCommand
 
-echo "Building for Windows"
+echo "Building for Windows amd64"
 $env:GOOS = "windows"
 
-$winCommand = 'go build -o buildOutput\castro.exe -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
+$winCommand = 'go build -o buildOutput\castro_win_amd64.exe -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
 
 iex $winCommand
 
-echo "Building for Linux"
+echo "Building for Linux amd64"
 $env:GOOS = "linux"
 
-$linuxCommand = 'go build -o buildOutput\castro'
+$linuxCommand = 'go build -o buildOutput\castro_linux_amd64 -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
 
 iex $linuxCommand
+
+echo "Building for Linux arm64"
+$env:GOOS = "linux"
+$env:GOARCH = "arm64"
+
+iex $linuxCommand
+
+
+
+
