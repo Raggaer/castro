06e2ce461f604999e9c6a27ee67df3ae459cd8db
Raggaer <nakotoffana@gmail.com>
2017-03-25 13:55:46 +0100

    Remove AppVeyor absolute path

--- appveyor_build.ps1
+++ appveyor_build.ps1
@@ -7,18 +7,18 @@ iex $initCommand
 echo "Building for Windows amd64"
 $env:GOOS = "windows"
 
-$winCommand = 'go build -v github.com/raggaer/castro -o buildOutput\castro_win_amd64.exe -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
+$winCommand = 'go build -v -o buildOutput\castro_win_amd64.exe -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
 
 iex $winCommand
 
 echo "Building for Linux amd64"
 $env:GOOS = "linux"
 
-$linuxCommand = 'go build -v github.com/raggaer/castro -o buildOutput\castro_linux_amd64 -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
+$linuxCommand = 'go build -v -o buildOutput\castro_linux_amd64 -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
 
 iex $linuxCommand
 
-$linuxCommand = 'go build -v github.com/raggaer/castro -o buildOutput\castro_linux_arm64 -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
+$linuxCommand = 'go build -v -o buildOutput\castro_linux_arm64 -ldflags "-X main.VERSION=$env:APPVEYOR_BUILD_VERSION -X main.BUILD_DATE=$date"'
 
 echo "Building for Linux arm64"
 $env:GOOS = "linux"
