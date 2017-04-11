0c8449369752cd2b6b133993efd7292f4123806a
Martin Nylind <martin@nylind.se>
2017-04-11 23:12:19 +0200

    Credits page (WIP)

---
+++ pages/credits/get.lua
@@ -0,0 +1,22 @@
+local contributors_url = "https://api.github.com/repos/Raggaer/castro/contributors"
+local commits_url = "https://api.github.com/repos/Raggaer/castro/commits?per_page=5"
+
+function get()
+    local data = cache:get("github")
+    if not data then
+        data = {}
+    	data.contributors = http:get(contributors_url)
+        data.commits = http:get(commits_url)
+        cache:set("github", data, "30m")
+    end
+
+    if data.contributors then
+        data.contributors = json:unmarshal(data.contributors).object
+    end
+
+    if data.commits then
+        data.commits = json:unmarshal(data.commits).object
+    end
+
+    http:render("credits.html", data)
+end
