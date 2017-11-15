782efd661d852f5270876f4dc120301d6aeeddd1
Raggaer <nakotoffana@gmail.com>
2017-11-15 17:57:38 +0100

    Update checkout history

--- pages/account/checkout/get.lua
+++ pages/account/checkout/get.lua
@@ -23,11 +23,27 @@ function get()
     end
 
     local account = session:loggedAccount()
-    local count = db:singleQuery("SELECT COUNT(*) as total FROM castro_shop_checkout WHERE account = ?", account.ID)
+    
+    local count = db:singleQuery([[
+        SELECT COUNT(*) as total 
+        FROM castro_shop_checkout a, players b, accounts c 
+        WHERE a.player = b.name AND b.account_id = c.id AND c.id = ?
+        ]], account.ID)
+
     local pg = paginator(page, 15, tonumber(count.total))
     local data = {}
 
-    data.list = db:query("SELECT a.name, b.given, b.amount FROM castro_shop_offers a, castro_shop_checkout b WHERE a.id = b.offer AND b.account = ? ORDER BY b.id DESC LIMIT ?, ?", account.ID, pg.offset, pg.limit)
+    data.list = db:query([[
+        SELECT a.name, b.given, b.amount 
+        FROM castro_shop_offers a, castro_shop_checkout b, accounts c, players d 
+        WHERE a.id = b.offer AND b.player = d.name AND d.account_id = c.id AND c.id = ?
+        ORDER BY b.id 
+        DESC LIMIT ?, ?
+        ]], 
+        account.ID, 
+        pg.offset, 
+        pg.limit
+    )
     data.paginator = pg
 
     http:render("checkouthistory.html", data)
