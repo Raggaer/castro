0d10e3a4cc3f7da61fb4a45af9fcc7062539d492
Sun <19597322+Sun27@users.noreply.github.com>
2018-11-22 19:54:44 +0100

    Update config.lua with correct values

--- pages/account/createchar/config.lua
+++ pages/account/createchar/config.lua
@@ -7,11 +7,11 @@ app.Custom.ValidVocationList = {1, 2, 3, 4}
 -- New character values
 app.Custom.NewCharacterValues = {
     level = 8,
-    experience = 4800,
-    health = 100,
-    healthmax = 100,
-    mana = 100,
-    manamax = 100,
-    cap = 100,
+    experience = 4200,
+    health = 185,
+    healthmax = 185,
+    mana = 35,
+    manamax = 35,
+    cap = 470,
     soul = 100
 }
