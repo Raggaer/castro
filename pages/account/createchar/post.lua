5180ca59df80e3121650a74435c3ed4a8fe4fb98
Sun <19597322+Sun27@users.noreply.github.com>
2018-11-23 01:20:07 +0100

    Updated to work with the more customizable config

--- pages/account/createchar/post.lua
+++ pages/account/createchar/post.lua
@@ -1,3 +1,11 @@
+local function fetchValues(voc)
+    local v = app.Custom.NewCharacterValues[voc]
+    if v.inherit_from ~= nil then
+        v = app.Custom.NewCharacterValues[v.inherit_from]
+    end
+    return v
+end
+
 function post()
     if not session:isLogged() then
         http:redirect("/")
@@ -48,20 +56,22 @@ function post()
         return
     end
 
+    ncv = fetchValues(tonumber(xml:vocationByName(http.postValues["character-vocation"]).ID)+1) -- new char values
+
     db:execute(
         "INSERT INTO players (name, sex, account_id, vocation, town_id, conditions, level, health, healthmax, mana, manamax, cap, soul) VALUES (?, ?, ?, ?, ?, '', ?, ?, ?, ?, ?, ?, ?)",
-        http.postValues["character-name"], 
-        http.postValues["character-gender"], 
-        account.ID, 
-        xml:vocationByName(http.postValues["character-vocation"]).ID, 
+        http.postValues["character-name"],
+        http.postValues["character-gender"],
+        account.ID,
+        xml:vocationByName(http.postValues["character-vocation"]).ID,
         otbm:townByName(http.postValues["character-town"]).ID,
-        app.Custom.NewCharacterValues.level,
-        app.Custom.NewCharacterValues.health,
-        app.Custom.NewCharacterValues.healthmax,
-        app.Custom.NewCharacterValues.mana,
-        app.Custom.NewCharacterValues.manamax,
-        app.Custom.NewCharacterValues.cap,
-        app.Custom.NewCharacterValues.soul
+        ncv.level,
+        ncv.health,
+        ncv.healthmax,
+        ncv.mana,
+        ncv.manamax,
+        ncv.cap,
+        ncv.soul
     )
 
     session:setFlash("success", "Character created")
