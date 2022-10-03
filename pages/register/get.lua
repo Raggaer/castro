09dcee6b60064fd12c8cdcbd2b2ab89cfc447e96
Martin Nylind <3932953+forgee@users.noreply.github.com>
2022-10-03 20:44:27 +0200

    Fix captcha

--- pages/register/get.lua
+++ pages/register/get.lua
@@ -8,6 +8,8 @@ function get()
 
     data["serverName"] = config:get("serverName")
     data["validationError"] = session:getFlash("validationError")
+    data["captchaEnabled"] = app.Captcha.Enabled
+    data["captchaKey"] = app.Captcha.Public
 
     http:render("register.html", data)
 end
